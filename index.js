9095306ecc2a3cb7f870c9994503f6f16db3f390
Kirill Malanyin <kmalanyin@appliedtech.ru>
2021-10-05 16:19:48 +0500

    fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const sw = navigator.serviceWorker.register('./service-worker.js');
 console.log(sw);
 
 const init = async () => {
-  const sw = await navigator.serviceWorker.ready();
+  const sw = await navigator.serviceWorker.ready;
   const push = await sw.pushManager.subscribe({
     userVisibleOnly: true,
     applicationServerKey: 'BNfHtKa0DJZlIGpqpNdhwWshIB_RnsnGd6xmG2IIG4l69Yby_eTPEp2awB2pOs6Vi3zMbq-xeK-HPATtGPrVDnM',
