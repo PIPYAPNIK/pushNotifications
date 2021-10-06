7f2186bcfe597d9e8fb06ecc5429e1e71d6d3528
Kirill Malanyin <kmalanyin@appliedtech.ru>
2021-10-06 16:54:43 +0500

    revert

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,9 @@
 self.addEventListener('push', function(event) {
   let message = event.data ? event.data.message : 'no payload';
 
-  const promiseChain = registration.getNotifications()
-    .then(notifications => {
-      let currentNotification;
-
-      for(let i = 0; i < notifications.length; i++) {
-        if (notifications[i].data) {
-          currentNotification = notifications[i];
-        }
-      }
-      return currentNotification;
-    }).then((currentNotification) => {
-      let notificationTitle;
-      const options = {};
-    
-      if (currentNotification) {
-        const messageCount = currentNotification.data.newMessageCount + 1;
-    
-        options.body = `You have ${messageCount} new messages.`;
-        options.data = {
-          newMessageCount: messageCount
-        };
-        notificationTitle = `New Messages`;
-    
-        currentNotification.close();
-      } else {
-        options.body = `"${message}"`;
-        options.data = {
-          newMessageCount: 1
-        };
-        notificationTitle = `New Message`;
-      }
-    
-      return registration.showNotification(
-        notificationTitle,
-        options,
-      );
-    });
-
-  event.waitUntil(promiseChain);
+  event.waitUntil(
+    self.registration.showNotification('It`s alive!!!!', {
+      body: message,
+    })
+  );
 });
